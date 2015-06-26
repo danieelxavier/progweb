56e37dc7466cb11a2b7966b710bb832f03f703e0
danieelxavier <danieelxavier@gmail.com>
2015-06-26 16:54:24 -0400

    Atividade do dia 24/062015 sobre PHP

---
+++ php1/processa.php
@@ -0,0 +1,6 @@
+<?php 
+	echo "<b>Nome: </b>" . $_POST["nome_completo"] . "<br/>";
+	echo "<b>Sexo: </b>" . $_POST["sexo"] . "<br/>";
+	echo "<b>Comentário: </b>" . $_POST["comentario"] . "<br/>";
+
+?>
